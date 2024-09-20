53dfaab9fec66b632118b05ada35f036c0df09af
Daniel <ddamek053@gmail.com>
2024-09-20 12:53:13 +0200

    firstcommit

--- main.c
+++ main.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
 
-int main(){
+int base()
+{
     for (int i = 0; i < 10; i++)
     {
         printf("\n");
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < 100; j++)
         {
-            printf("\033[1;31m%c\033[0m" , 219);
+            if (i > 5)
+            {
+                printf("\033[1;31m%c\033[0m", 219);
+            }
+            else if( i == 5 && j == 7)
+            {
+                printf("#");
+            }
+            else
+            {
+                printf("1");
+            }
         }
-        
     }
-    
+    return 0;
+}
+
+int main()
+{
+    base();
     return 0;
 }
